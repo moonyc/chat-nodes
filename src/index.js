816f33320199ca0e7610d483e616c400ad892ac3
muni <booscarface@gmail.com>
2023-07-12 22:00:22 +0200

    established websocket connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
-
+const socketio = require('socket.io')
 const app = express()
 const server = http.createServer(app)
-
+const io = socketio(server)
 const dir = path.join(__dirname, '../public')
 
 const port = process.env.PORT || 3000
@@ -13,6 +13,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(dir)) // app.use() mounts a specified middleware function at the specified path
 
+io.on('connection', () => {
+    console.log('New WebSocket Connection')
+})
 
 server.listen(port, () => {
     console.log('Server is up on port ' + port)
